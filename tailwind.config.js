2714e62f8b3fc56feaaea51ba2edf6b00ed8bacf
Vitor Lima <vitornascimento923@gmail.com>
2024-09-19 00:13:03 -0300

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#038B86",
+        secondary: "#025956",
+      },
+    },
+  },
+  plugins: [],
+};
